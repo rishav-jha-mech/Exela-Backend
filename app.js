b3ff0d33b01f3523b1ca7264c85026e2350bf2a8
rishav-jha-mech <rishav.rj74@gmail.com>
2022-07-15 00:19:23 +0530

    Changes for deployment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 require('./db/conf');
 
 app.use(express.json());
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 
 app.use('/',billsRouter)
