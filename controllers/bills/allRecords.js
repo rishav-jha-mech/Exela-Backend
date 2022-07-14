ba936d8c322a708a4d28693eba59cc0e092f56a0
rishav-jha-mech <rishav.rj74@gmail.com>
2022-07-14 23:51:55 +0530

    Working correctly to list all the bills

--- controllers/bills/allRecords.js
+++ controllers/bills/allRecords.js
@@ -2,10 +2,10 @@ const { db } = require('../../models/electricityBillSchema');
 const BillSchema = require('../../models/electricityBillSchema');
 
 const allRecords = async (req, res) => {
-    const { page = 1, limit = 10, sort = -1 } = req.query  //seting default value of page 1 and limit = 2 sort is either 1 or -1
+    const { page = 1, limit = 9, sort = -1, sort_type='bill_date' } = req.query  //seting default value of page 1 and limit = 2 sort is either 1 or -1
     await db.collection('bills')
             .find()
-            .sort('bill_date', sort)
+            .sort(sort_type, sort)
             .limit(limit * 1)
             .skip((page-1) * limit)
             .toArray()
